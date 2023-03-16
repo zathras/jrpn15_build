1b3b0b3a6673065012e0839e9b11a87cde664e0a
Bill Foote <billf@jovial.com>
2023-03-15 20:06:01 -0700

    Change to new logo (Jupiter with 15C)

---
+++ android/app/src/main/kotlin/com/example/jrpn15/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jovial.jrpn15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,4 +3,3 @@ import 'package:jrpn15/main.dart' as jrpn15;
 void main(List<String> args) {
   jrpn15.main(args);
 }
-
